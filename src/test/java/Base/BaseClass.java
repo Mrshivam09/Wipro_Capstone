590e63659f63c86ac113b6b8482c4264ce13ae1e
Mrshivam09 <125801826+Mrshivam09@users.noreply.github.com>
2026-06-13 16:29:03 +0530

    Update BaseClass.java

--- src/test/java/Base/BaseClass.java
+++ src/test/java/Base/BaseClass.java
@@ -4,7 +4,9 @@ import java.time.Duration;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
@@ -16,15 +18,35 @@ public class BaseClass {
 
         System.out.println("Launching Browser : " + browser);
 
+        // Read execution mode from Maven command
+        String executionMode = System.getProperty("executionMode", "local");
+        boolean isHeadless = executionMode.equalsIgnoreCase("jenkins");
+
         if (browser.equalsIgnoreCase("chrome")) {
 
             WebDriverManager.chromedriver().setup();
-            driver = new ChromeDriver();
+
+            ChromeOptions options = new ChromeOptions();
+
+            if (isHeadless) {
+                options.addArguments("--headless=new");
+                options.addArguments("--disable-gpu");
+                options.addArguments("--window-size=1920,1080");
+            }
+
+            driver = new ChromeDriver(options);
 
         } else if (browser.equalsIgnoreCase("firefox")) {
 
             WebDriverManager.firefoxdriver().setup();
-            driver = new FirefoxDriver();
+
+            FirefoxOptions options = new FirefoxOptions();
+
+            if (isHeadless) {
+                options.addArguments("-headless");
+            }
+
+            driver = new FirefoxDriver(options);
 
         } else {
 
@@ -32,9 +54,7 @@ public class BaseClass {
         }
 
         driver.manage().window().maximize();
-
-        driver.manage().timeouts()
-                .implicitlyWait(Duration.ofSeconds(10));
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 
         driver.get("https://blazedemo.com/");
     }
